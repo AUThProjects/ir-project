a6f6a471c5609c52d93aff31c4af06f17f19d805
Steve Laskaridis <stevelaskaridis@gmail.com>
2017-02-17 16:24:47 +0200

    Refactor classes to be presentable

--- src/main/java/gr/auth/csd/IRProject/supervised/LRAlgorithm.java
+++ src/main/java/gr/auth/csd/IRProject/supervised/LRAlgorithm.java
@@ -3,66 +3,100 @@ package gr.auth.csd.IRProject.supervised;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.spark.ml.Model;
 import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator;
 import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.tuning.CrossValidator;
 import org.apache.spark.ml.tuning.CrossValidatorModel;
 import org.apache.spark.ml.tuning.ParamGridBuilder;
-import org.apache.spark.mllib.classification.SVMWithSGD;
-import org.apache.spark.mllib.optimization.L1Updater;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
 
 import java.util.Arrays;
 
 /**
- * Created by steve on 03/01/2017.
+ * Logistic regression wrapper class
  */
 public class LRAlgorithm {
-    public static void main(String[] args) {
-        String inputDirectory = "src/main/resources/tfIdfData.json";
-        String outputDirectory = "src/main/resources/lrModel";
-        SparkSession spark = SparkSession.builder()
-                .appName("IRProjectKMeans")
-                .getOrCreate();
-        Logger logger = LogManager.getRootLogger();
+    /**
+     * The output directory of the trained model.
+     */
+    static String outputModelDirectory = "src/main/resources/lrModel";
+    static String inputLabelledDirectory = "src/main/resources/tfIdfData.json";
+    static String inputUnlabelledDirectory = "src/main/resources/tfIdfDataUnlabelled.json";
+    static String outputDataDirectory = "src/main/resources/predictions.txt";
+    static Logger logger;
+
+    static {
+        logger = LogManager.getRootLogger();
         logger.setLevel(Level.WARN);
-        Dataset<Row> data = spark.read().parquet(inputDirectory);
-        
-        Dataset<Row>[] datasets = data.randomSplit(new double[]{0.9,0.1}, 123321);
+    }
+
+    /**
+     * Execution entrypoint.
+     * @param args CLI arguments
+     */
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("IRProjectLogisticRegression")
+                .getOrCreate();
+
+
+        Dataset<Row> labelledData = spark.read().parquet(inputLabelledDirectory);
+        train(labelledData, new double[]{0.9,0.1}, 123321);
+
+        Dataset<Row> unlabelledData = spark.read().parquet(inputUnlabelledDirectory);
+        test(unlabelledData, outputDataDirectory);
+    }
+
+    /**
+     * Method training the model using Cross Validation.
+     * @param data The labelled data to be trained on
+     * @param split Percentages split on holdout
+     * @param seed Seed for randomness
+     */
+    public static void train(Dataset<Row> data, double[] split, int seed) {
+        Dataset<Row>[] datasets = data.randomSplit(split, seed);
         Dataset<Row> trainSet = datasets[0];
         Dataset<Row> testSet = datasets[1];
 
         LogisticRegression lr = new LogisticRegression().setPredictionCol("prediction").setFeaturesCol("features").setMaxIter(200);
         ParamMap[] paramGrid = new ParamGridBuilder().build();
-//        ParamMap[] paramGrid = new ParamGridBuilder()
-//                .addGrid(lr.regParam(), new double[] {0.1, 0.01})
-//                .addGrid(lr.elasticNetParam(), new double[] {0.1, 0.01})
-//                .build();
 
 
         CrossValidator cv = new CrossValidator()
                 .setEstimator(lr.setRegParam(0.05))
                 .setEvaluator(new BinaryClassificationEvaluator().setRawPredictionCol("prediction"))
-                .setEstimatorParamMaps(paramGrid).setNumFolds(3);  // Use 3+ in practice
+                .setEstimatorParamMaps(paramGrid).setNumFolds(3);
 
         CrossValidatorModel cvModel = cv.fit(trainSet);
 
+        Dataset<Row> predictions = cvModel.transform(testSet);
+
+        logger.log(Level.WARN, Arrays.toString(cvModel.avgMetrics()));
+
+
+        // Now train on the whole dataset
+        cvModel = cv.fit(data);
+
         try {
-            cvModel.save(outputDirectory);
+            cvModel.save(outputModelDirectory);
         }
         catch (java.io.IOException e) {
             logger.log(Level.ERROR, e.getMessage());
         }
+    }
 
-        Dataset<Row> predictions = cvModel.transform(testSet);
-        predictions.show(1000);
+    /**
+     * Method predicting labels on dataset.
+     * @param data Tha unlabelled data to label
+     * @param outputDataDirectory The output data directory
+     */
+    public static void test(Dataset<Row> data, String outputDataDirectory) {
+        CrossValidatorModel cvModel = CrossValidatorModel.load(outputModelDirectory);
+        Dataset<Row> predictions = cvModel.transform(data);
 
-
-        logger.log(Level.WARN, Arrays.toString(cvModel.avgMetrics()));
+        predictions.select("id", "prediction").toJavaRDD().coalesce(1).saveAsTextFile(outputDataDirectory);
     }
 }
--- src/main/java/gr/auth/csd/IRProject/transform/Word2Vector.java
+++ src/main/java/gr/auth/csd/IRProject/transform/Word2Vector.java
@@ -10,30 +10,48 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 /**
- * Created by steve on 09/02/2017.
+ * Word2Vec class for transforming features to concepts and reducing dimensions
  */
 public class Word2Vector {
+
+    static Logger logger;
+
+    static {
+        logger = LogManager.getRootLogger();
+        logger.setLevel(Level.WARN);
+    }
+
+    /**
+     * Execution entrypoint.
+     * @param args CLI args
+     */
     public static void main(String[] args) {
         String inputDirectory = "src/main/resources/tfIdfData.json";
         String outputDirectory = "src/main/resources/w2vData";
+        final int vectorSize = 50;
+
         SparkSession spark = SparkSession.builder()
                 .appName("IRProjectKMeans")
                 .getOrCreate();
-        Logger logger = LogManager.getRootLogger();
-        logger.setLevel(Level.WARN);
         Dataset<Row> data = spark.read().parquet(inputDirectory);
 
+        Dataset<Row> transformed = word2vecTransform(spark, data, vectorSize);
+        transformed.write().parquet(outputDirectory);
+    }
+
+    /**
+     * Method responsible for doing the w2v transformation.
+     * @param spark The spark session object
+     * @param data The dataset to be transformed
+     * @return The transformed dataset
+     */
+    public static Dataset<Row> word2vecTransform(SparkSession spark, Dataset<Row> data, int vectorSize) {
         Word2Vec word2Vec = new Word2Vec()
                 .setInputCol("woutSWords")
                 .setOutputCol("w2vRes")
-                .setVectorSize(50)
+                .setVectorSize(vectorSize)
                 .setMinCount(0);
-
         Word2VecModel model = word2Vec.fit(data);
-
-
-        Dataset<Row> transformed =  model.transform(data);
-        transformed.write().parquet(outputDirectory);
-
+        return model.transform(data);
     }
 }
